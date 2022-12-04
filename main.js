fc5d1a0db8cae145a8b67fb4011138ee21266984
Nathan Prieto <ncprieto@ucsc.edu>
2022-12-04 02:15:53 -0800

    graph colors now line up with map colors

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//let filePath = "./trails-topojson-detailed/"
 let filePath = "./trails-topojson-simplified/"
 let trails = ["north-country-trail.json", "pacific-crest-trail.json", "appalachian-trail.json", "continental-divide-trail.json", 
               "arizona-national-scenic-trail.json", "batona-trail.json", "benton-mackaye-trail.json", 
@@ -6,47 +5,55 @@ let trails = ["north-country-trail.json", "pacific-crest-trail.json", "appalachi
               "new-england-trail.json", "northville-placid-trail.json", "john-muir-trail.json", "big-SEKI-loop.json",
               "pinhoti-trail.json", "tahoe-rim-trail.json", "tuscarora-trail.json", "wonderland-trail.json"];
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+let trailNames = ["North Country National Scenic Trail", "Pacific Crest Trail", "The Appalachian Trail", "Continental Divide National Scenic Trail",
+                  "Arizona National Scenic Trail", "Batona Trail", "Benton-MacKaye Trail", "Colorado Trail", "Laurel Highlands Hiking Trail",
+                  "Long Path Trail", "The Long Trail", "New England Trail", "Northville Placid Trail", "John Muir Trail", "Big SEKI Loop",
+                  "Pinhoti Trail", "Tahoe Rim Trail", "Tuscarora Trail", "The Wonderland Trail"];
 
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+// map names to colors
+let nameToColor = {};
+for(let i in trailNames){
+    nameToColor[trailNames[i]] = color(trailNames[i])
+};
+
+// VARIABLES RELATED TO THE MAP
 // this code was taken from a StackOverflow response
 // which can be found here https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
 d3.select("div#map")
     .append("div")
     .classed("svg-container", true) 
     .append("svg")
+    .attr("id", "map")
     .attr("preserveAspectRatio", "xMinYMin meet")
     .attr("viewBox", "0 0 1020 960")
     .classed("svg-content-responsive", true)
 
-var svg = d3.select("svg");
-var width = $("svg").parent().width();
+var mapSvg = d3.select("svg#map");
+var mapWidth = $("svg#map").parent().width();
 
 let projection = d3.geoAlbersUsa()
                     .scale(1400)
-                    .translate([width / 2.25, width / 2.65]);
+                    .translate([mapWidth / 2.25, mapWidth / 2.65]);
 
 let path = d3.geoPath()
                 .pointRadius(0)
                 .projection(projection);
 
-var zoom = d3.zoom()
+var mapZoom = d3.zoom()
     .scaleExtent([1, 12])
-    .translateExtent([[-90, -25], [width + 90, width - 40]])
-    .on("zoom", zoomed);
+    .translateExtent([[-90, -25], [mapWidth + 90, mapWidth - 40]])
+    .on("zoom", mapZoomed);
 
 var tooltip = d3.select("body")
     .append("div")
     .attr("class", "mapTooltip")
     .style("opacity", 0);
 
-// draw us map
-d3.queue()
-    .defer(d3.json, "./USA.json")
-    .await(drawUS);
-
-function drawUS(error, us){
+// ACTUALLY DRAWING THE MAP
+d3.json("./USA.json", function(error, us){
     if(error) throw error;
-    svg.append("g")
+    mapSvg.append("g")
         .attr("class", "USA")
         .selectAll("path")
         .data(topojson.feature(us, us.objects.units).features)
@@ -55,39 +62,34 @@ function drawUS(error, us){
         .attr("stroke-linejoin", "round")
         .attr("fill", "none")
         .attr("d", path);
-}
+});
 
+// ACTUALLY DRAWING THE TRAILS
 for(let i in trails){
-    // draw trail
-    d3.queue()
-        .defer(d3.json, filePath + trails[i])
-        .await(drawTrail);
+    d3.json(filePath + trails[i], function(error, trail){
+        if(error) throw error;
+        mapSvg.append("g")
+            .attr("class", "trail")
+            .selectAll("path")
+            .data(topojson.feature(trail, trail.objects.trail).features)
+            .enter().append("path")
+            .attr("stroke", color(trail.name))
+            .attr("d", path)
+            .on("mouseover", function(d){
+                tooltip.style("opacity", 1)
+                        .style("left", (d3.event.pageX + 10) + "px")
+                        .style("top", (d3.event.pageY - 20) + "px");
+                tooltip.html(trail.name + "<br>" + "Total Distance: " + trail.distance + " Miles<br>" + "Max Elevation: " + trail.maxElev + " Meters");
+                }
+            )
+            .on("mouseout", function(d){
+                tooltip.style("opacity", 0);
+            });
+    })
 }
 
-function drawTrail(error, trail){
-    if(error) throw error;
-
-    svg.append("g")
-        .attr("class", "trail")
-        .selectAll("path")
-        .data(topojson.feature(trail, trail.objects.trail).features)
-        .enter().append("path")
-        .attr("stroke", color(trail.id))
-        .attr("d", path)
-        .on("mouseover", function(d){
-            tooltip.style("opacity", 1)
-                    .style("left", (d3.event.pageX + 10) + "px")
-                    .style("top", (d3.event.pageY - 20) + "px");
-            tooltip.html(trail.name + "<br>" + "Total Distance: " + trail.distance + " Miles<br>" + "Max Elevation: " + trail.maxElev + " Meters");
-            }
-        )
-        .on("mouseout", function(d){
-            tooltip.style("opacity", 0);
-        });
-}
-
-svg.call(zoom);
-function zoomed(){
-    svg.selectAll(".USA").attr("transform", d3.event.transform);
-    svg.selectAll(".trail").attr("transform", d3.event.transform);
+mapSvg.call(mapZoom);
+function mapZoomed(){
+    mapSvg.selectAll(".USA").attr("transform", d3.event.transform);
+    mapSvg.selectAll(".trail").attr("transform", d3.event.transform);
 }
